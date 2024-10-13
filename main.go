0f8316012563fff96645f583da51e6ed139a9372
Elias R. <54182601+Roshick@users.noreply.github.com>
2024-10-13 22:10:02 +0200

    Implement module

---
+++ main.go
@@ -0,0 +1,99 @@
+package caddy_module_github_webhook
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	caddy.RegisterModule(Middleware{})
+	httpcaddyfile.RegisterHandlerDirective("validate_github_webhook_payload", parseCaddyfile)
+}
+
+// Middleware implements an HTTP handler.
+type Middleware struct {
+	Secret string `json:"secret,omitempty"`
+}
+
+// CaddyModule returns the Caddy module information.
+func (Middleware) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.github_webhook_validation_payload",
+		New: func() caddy.Module { return new(Middleware) },
+	}
+}
+
+// Validate implements caddy.Validator.
+func (m *Middleware) Validate() error {
+	if m.Secret == "" {
+		return fmt.Errorf("github webhook secret is empty")
+	}
+
+	return nil
+}
+
+// ServeHTTP implements caddyhttp.MiddlewareHandler.
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	payloadBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		// bad request in case of payload error
+		w.WriteHeader(400)
+		_, err = w.Write(nil)
+		return err
+	}
+
+	actual := []byte(strings.TrimPrefix(r.Header.Get("X-Hub-Signature-256"), "sha256="))
+
+	mac := hmac.New(sha256.New, []byte(m.Secret))
+	mac.Write(payloadBytes)
+	expected := mac.Sum(nil)
+
+	valid := subtle.ConstantTimeCompare(expected, actual)
+	if valid == 0 {
+		// unauthorized in case of invalid signature
+		w.WriteHeader(401)
+		_, err = w.Write(nil)
+		return err
+	}
+
+	// pass to the next handler
+	return next.ServeHTTP(w, r)
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
+func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	// consume directive name
+	d.Next()
+
+	// require an argument
+	if !d.NextArg() {
+		return d.ArgErr()
+	}
+
+	// store the argument
+	m.Secret = d.Val()
+	return nil
+}
+
+// parseCaddyfile unmarshals tokens from h into a new Middleware.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m Middleware
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
+}
+
+// Interface guards
+var (
+	_ caddy.Validator             = (*Middleware)(nil)
+	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
+	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
+)
